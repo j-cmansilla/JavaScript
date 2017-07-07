83e7df74f459a006760f8f4e831986e8c7d887fc
Unknown <jcmansillam@correo.url.edu.gt>
2017-07-07 17:43:36 -0600

    EsLint Added
    I added EsLint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css'
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('Q0.00');
-console.log(`Voy a pagar ${courseValue} por este curso!`);
+console.log(`Voy a pagar ${courseValue} por este curso!`); // eslint-disable-line no-console
